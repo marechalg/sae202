a29f52bc96cf0cbd8a0e458dc15ede33ab8638ef
titouanlrnt <tlouisre56730@gmail.com>
2025-03-28 17:46:20 +0100

    Kmeans

--- Calculs.py
+++ Calculs.py
@@ -1,28 +1,10 @@
 from math import *
 from matplotlib import pylab as plt
 import numpy as np
-<<<<<<< HEAD
 
 import random
 # from sklearn.preprocessing import MinMaxScaler
 # from sklearn.cluster import KMeans
-
-
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.cluster import KMeans
-
-
-import random
-# from sklearn.preprocessing import MinMaxScaler
-# from sklearn.cluster import KMeans
-
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.cluster import KMeans
-=======
-import random
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.cluster import KMeans
->>>>>>> 321c0409f32f902798b582c3ed7da014e43cc298
 
 letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
@@ -109,16 +91,57 @@ class Calculs(object):
         
         return col
     
-<<<<<<< HEAD
-    def kMeans(self, x, y):
-        
-        matr = 
-        
-=======
-      def listeToMatr(self, x, y):
+    def listeToMatr(self, x, y):
         matr = []
         for i in range(len(x)):
             matr.append([x[i], y[i]])
         return matr
     
->>>>>>> 321c0409f32f902798b582c3ed7da014e43cc298
+
+    # def Kmeans(self, x, y):
+    #     # On choisira dans un premier temps $k$ au hasard et on se donne un jeu de données (une matrice où chaque ligne représente une donnée).
+    #     listCentres = []
+    #     groupes = []
+    #     arret = false
+    #     matr = self.listeToMatr(x, y)
+    #     k = random.randint(1, len(matr))
+    #     for i in range(k):
+    #         n = random.randint(0, len(matr) - 1)
+    #         listCentres.append([x[n], y[n]])
+    #     while(arret != true):
+    #         for i in range(len(matr)):
+    #             for centres in listCentres:
+    #                 mini = x[0]
+    #                 for j in range(len(listCentres)):
+    #                     dist = self.Minkowski(matr[i], centres, 2)
+    #                     if dist < mini:
+    #                         mini = dist
+    #                         numCentre = j
+    #             groupes[numCentre].append(matr[i])
+    #     for t in range(len(groupes)):
+    #         centroide = np.mean(groupes[t], axis=0)         
+                
+
+    def kmeans(matr, k, max_iters=100, tol=1e-4):
+
+        # Initialisation des centroides aléatoirement
+        indices = np.random.choice(len(matr), k, replace=False)
+        centroids = matr[indices]
+
+        for _ in range(max_iters):
+            # Calcul des distances entre chaque point et les centroides
+            distances = np.linalg.norm(matr[:, np.newaxis] - centroids, axis=2)
+            
+            # Assignation des points au centroïde le plus proche
+            labels = np.argmin(distances, axis=1)
+            
+            # Mise à jour des centroides
+            new_centroids = np.array([matr[labels == i].mean(axis=0) for i in range(k)])
+            
+            # Vérifier la convergence
+            if np.linalg.norm(new_centroids - centroids) < tol:
+                break
+
+            centroids = new_centroids
+        
+        return labels, centroids
