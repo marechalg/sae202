bad676ac0d8dd9f972f242592339caac830ce094
marechalg <guewenmarechal@protonmail.com>
2025-03-28 16:44:01 +0100

    msg

--- Calculs.py
+++ Calculs.py
@@ -98,7 +98,6 @@ class Calculs(object):
         return res
     
     def tri(self, col = {}):
-<<<<<<< HEAD
         sorte = []
         while bool(col):
             mini = max(col)
@@ -106,46 +105,3 @@ class Calculs(object):
             print(mini)
         
         return col
-=======
-<<<<<<< HEAD
-        for i in range(1,len(col)):
-            for j in range(1,len(col)):
-=======
-        for i in range(1, len(col)):
-            for j in range(1, len(col)):
->>>>>>> 12490a65a3db1f486fc6a83c106e6e180b570fb3
-                if col[j] < col[i]:
-                    temp = col[i]
-                    col[j] = col[i]
-                    col[i] = temp
-        return col
-    
-    
-    
-    def kMeans(self, x, y):
-        # On choisira dans un premier temps $k$ au hasard et on se donne un jeu de données (une matrice où chaque ligne représente une donnée).
-
-        for 
-
-        miniPts = 
-        maxiPts = 
-        k = random.randint(miniPts, maxiPts)
-
-
-
-<<<<<<< HEAD
-
-    def listeToMatr(self, x, y):
-        matr = []
-        for i in range(len(x)):
-            matr.append([x[i], y[i]])
-        return matr
-    
-    
-
-=======
-        #Ensuite, Choisir aléatoirement $k$ points,  ces points sont les centres des clusters (appelés aussi centroïd), puis on répète :  
-        #1. Affecter chaque point (ligne de  la matrice de données) au groupe dont il est le plus proche (de son centre).  
-        #2. Recalculer le centre de chaque cluster et modifier le centroide. On prendra simplement la moyenne des points du cluster.
->>>>>>> 12490a65a3db1f486fc6a83c106e6e180b570fb3
->>>>>>> 4b9a9240cce5531a6e19ee4a3b7d0d05bbd0931c
